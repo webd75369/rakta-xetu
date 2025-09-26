b9cdce86e270339a394ae3a37d3b7dcf80087b4d
Nilav <nilavtalukdar06@gmail.com>
2025-09-26 19:16:39 +0530

    added login and logout functionality

---
+++ src/lib/auth.ts
@@ -0,0 +1,35 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { nextCookies } from "better-auth/next-js";
+
+const client = new MongoClient("mongodb://localhost:27017/database");
+const db = client.db();
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    client,
+  }),
+  socialProviders: {
+    google: {
+      prompt: "select_account",
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
+  user: {
+    additionalFields: {
+      isUser: {
+        type: "boolean",
+        required: true,
+        defaultValue: false,
+      },
+      isDonor: {
+        type: "string",
+        required: true,
+        defaultValue: true,
+      },
+    },
+  },
+  plugins: [nextCookies()],
+});
