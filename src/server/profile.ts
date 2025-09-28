ab87e0210a97869c6ac07fad595c7b235294c14b
Nilav <nilavtalukdar06@gmail.com>
2025-09-28 13:50:10 +0530

    made a new server action for getting the charts info

--- src/server/profile.ts
+++ src/server/profile.ts
@@ -1,5 +1,6 @@
 "use server";
 import connectToDb from "@/db";
+import { Blood } from "@/db/models/blood";
 import Profile from "@/db/models/profile";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
@@ -24,6 +25,43 @@ export const getProfileInfo = async () => {
     console.error(error);
     return {
       message: "failed to get name",
+    };
+  }
+};
+
+export const chartInfo = async () => {
+  try {
+    const session = await auth.api.getSession({
+      headers: await headers(),
+    });
+    if (!session) {
+      throw new Error("the user is not authenticated");
+    }
+    await connectToDb();
+    const totalUnitsDonated = await Blood.find({
+      isAccepted: true,
+      acceptedBy: session.user.id,
+    });
+    const totalUnitsRequested = await Blood.find({
+      isAccepted: false,
+      userId: session.user.id,
+    });
+    const totalLivesAffected = await Blood.find({ userId: session.user.id });
+    return {
+      totalUnitsDonated: totalUnitsDonated.reduce((acc, curr) => acc + curr, 0),
+      totalUnitsRequested: totalUnitsRequested.reduce(
+        (acc, curr) => acc + curr,
+        0
+      ),
+      totalLivesAffected: totalLivesAffected.reduce(
+        (acc, curr) => acc + curr,
+        0
+      ),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      message: "failed to get chart info",
     };
   }
 };
