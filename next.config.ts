8e5f6a7f50ef7dff6bb0e7c38bab774bee7e09c3
Nilav Talukdar <nilavtalukdar06@gmail.com>
2025-10-04 09:03:27 +0530

    updated roote configurations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import { withHydrationOverlay } from "@builder.io/react-hydration-overlay/next";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: []
+  }
+};
 
 export default withHydrationOverlay({
   appRootSelector: "main",
