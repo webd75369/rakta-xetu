c58465e46665da026f2b58ad972e342916336f22
Nilav Talukdar <nilavtalukdar06@gmail.com>
2025-10-07 10:50:03 +0530

    completed scheduling process

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,8 +2,9 @@ import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
 import { healthCheck } from "@/inngest/functions/health";
 import { sendEmail } from "@/inngest/functions/send-email";
+import { sendConfirmationEmail } from "@/inngest/functions/send-confirmation-email";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [healthCheck, sendEmail],
+  functions: [healthCheck, sendEmail, sendConfirmationEmail],
 });
