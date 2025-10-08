19b65710bfd05c642b1f3383d1bd4c661bd05dc2
Nilav Talukdar <nilavtalukdar06@gmail.com>
2025-10-08 16:23:02 +0530

    added UI for zero users

--- src/components/modules/chat/users.tsx
+++ src/components/modules/chat/users.tsx
@@ -1,7 +1,15 @@
 "use client";
 import { IUser } from "../../../../types/schema";
 
-export function UsersComponent({ users }: { users: IUser }) {
+export function UsersComponent({ users }: { users: IUser[] }) {
+  if (users?.length === 0) {
+    return (
+      <p className="font-light text-neutral-500 max-w-md mx-auto my-4 text-center">
+        No users are present at this moment
+      </p>
+    );
+  }
+
   return (
     <div className="max-w-md mx-auto my-4">
       <p>Users</p>
