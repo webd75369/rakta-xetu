ccdf9727f763ec8251b6e61dab57146a57ed26d3
Nilav Talukdar <nilavtalukdar06@gmail.com>
2025-10-12 21:57:32 +0530

    fetched donors

--- src/components/modules/donors/donors-list.tsx
+++ src/components/modules/donors/donors-list.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { IUser } from "../../../../types/schema";
 import initials from "initials";
 
-export function DonorsList({ donors }: { donors: IUser[] }) {
+export function DonorsList({ donors }: { donors: any }) {
+  console.log(donors);
   if (donors?.length === 0) {
     return <p className="text-red-500 font-light">No donors are present</p>;
   }
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center gap-4">
-      {donors.map((donor: IUser) => (
+    <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center gap-4 cursor-pointer">
+      {donors.map((donor: any) => (
         <div
           className="p-3 rounded-lg border hover:bg-sidebar transition-all w-full"
           key={donor._id?.toString()}
@@ -18,22 +18,24 @@ export function DonorsList({ donors }: { donors: IUser[] }) {
           <div className="w-full flex items-center justify-between">
             <div className="flex justify-center items-center gap-x-4">
               <Avatar>
-                <AvatarImage src={donor.image} />
-                <AvatarFallback>{initials(donor.name)}</AvatarFallback>
+                <AvatarImage src={donor.user.image} />
+                <AvatarFallback>{initials(donor.user.name)}</AvatarFallback>
               </Avatar>
               <div className="flex flex-col items-start justify-between">
                 <p className="text-sm text-neutral-500 font-light">
-                  {donor.name}
+                  {donor.user.name}
                 </p>
-                <p className="text-xs text-neutral-400 font-extralight">
-                  {donor.email}
+                <p className="text-xs text-neutral-400 font-extralight max-w-[160px] truncate">
+                  {donor.location}
                 </p>
               </div>
             </div>
             <div className="flex flex-col items-end justify-between">
-              <p className="text-sm text-rose-500 font-light">B+</p>
+              <p className="text-sm text-rose-500 font-light">
+                {donor.bloodGroup}
+              </p>
               <p className="text-xs text-neutral-500 font-extralight">
-                Contact
+                {donor.gender}
               </p>
             </div>
           </div>
