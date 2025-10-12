4c51f564ade42ad2329e7d30a1f2e6907179aa09
Nilav Talukdar <nilavtalukdar06@gmail.com>
2025-10-12 21:26:18 +0530

    updated server action to fetch the donors

--- src/server/donors/fetch-donors.ts
+++ src/server/donors/fetch-donors.ts
@@ -11,17 +11,44 @@ export const fetchDonors = async () => {
     const session = await auth.api.getSession({ headers: await headers() });
     if (!session) throw new Error("the user is not authenticated");
     await connectToDb();
-    const result = await db
-      .collection("user")
-      .find({ _id: { $ne: new ObjectId(session.user.id) }, isDonor: true })
-      .sort({ createdAt: -1 })
+    const donors = await db
+      .collection("profiles")
+      .aggregate([
+        {
+          $lookup: {
+            from: "user",
+            localField: "userId",
+            foreignField: "_id",
+            as: "user",
+          },
+        },
+        {
+          $unwind: {
+            path: "$user",
+            preserveNullAndEmptyArrays: false,
+          },
+        },
+        {
+          $match: {
+            "user.isDonor": true,
+            "user._id": { $ne: new ObjectId(session.user.id) },
+          },
+        },
+        {
+          $sort: { "user.createdAt": -1 },
+        },
+        {
+          $project: {
+            __v: 0,
+            "user.password": 0,
+            "user.__v": 0,
+          },
+        },
+      ])
       .toArray();
-    const donors = JSON.parse(JSON.stringify(result));
-    return donors;
+    return JSON.parse(JSON.stringify(donors));
   } catch (error) {
     console.error(error);
-    return {
-      message: "failed to fetch donors",
-    };
+    return { message: "failed to fetch donors" };
   }
 };
