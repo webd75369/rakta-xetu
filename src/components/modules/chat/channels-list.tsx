422749cd63a5a54d5759b31212ec1e8c8663de8d
Nilav Talukdar <nilavtalukdar06@gmail.com>
2025-10-12 13:14:32 +0530

    added channel list

--- src/components/modules/chat/channels-list.tsx
+++ src/components/modules/chat/channels-list.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { type User } from "stream-chat";
+import { Chat, ChannelList, useCreateChatClient } from "stream-chat-react";
+import "stream-chat-react/dist/css/v2/index.css";
 
 interface Props {
   user: User;
@@ -8,9 +11,33 @@ interface Props {
 }
 
 export function ChannelsList({ user, token }: Props) {
+  const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY!;
+  const userId = user.id.toString();
+  const userToken = token;
+
+  const filters = { members: { $in: [userId] }, type: "messaging" };
+  const options = { presence: true, state: true };
+  const sort = { last_message_at: -1 } as any;
+
+  const client = useCreateChatClient({
+    apiKey,
+    tokenOrProvider: userToken,
+    userData: { id: userId },
+  });
+
+  if (!client) {
+    return (
+      <div className="max-w-md mx-auto w-full flex justify-center items-center my-4">
+        <Spinner />
+      </div>
+    );
+  }
+
   return (
     <div className="max-w-md mx-auto w-full">
-      <p className="text-neutral-500 text-center font-light">Channels List</p>
+      <Chat client={client}>
+        <ChannelList filters={filters} options={options} sort={sort} />
+      </Chat>
     </div>
   );
 }
