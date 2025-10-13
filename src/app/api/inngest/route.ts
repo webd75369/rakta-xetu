9fc664d413d68bdf176de6922580b0608ebc3e8b
Nilav Talukdar <nilavtalukdar06@gmail.com>
2025-10-13 23:17:02 +0530

    added inngest function for sending the emails

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -3,8 +3,9 @@ import { inngest } from "@/inngest/client";
 import { healthCheck } from "@/inngest/functions/health";
 import { sendEmail } from "@/inngest/functions/send-email";
 import { sendConfirmationEmail } from "@/inngest/functions/send-confirmation-email";
+import { sendRequest } from "@/inngest/functions/send-request";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [healthCheck, sendEmail, sendConfirmationEmail],
+  functions: [healthCheck, sendEmail, sendConfirmationEmail, sendRequest],
 });
---
+++ src/inngest/functions/send-request.ts
@@ -0,0 +1,23 @@
+import { inngest } from "../client";
+import { SendRequest } from "@/components/emails/send-request";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY!);
+
+export const sendRequest = inngest.createFunction(
+  { id: "send-request" },
+  { event: "send/request" },
+  async ({ event }) => {
+    const { data } = await resend.emails.send({
+      from: `RaktaXetu <${process.env.SMTP_DOMAIN!}>`,
+      to: [event.data.email],
+      subject: "Donation Request",
+      react: SendRequest({
+        requestor: event.data.requestor,
+        email: event.data.email,
+        image: event.data.image,
+      }),
+    });
+    return data;
+  }
+);
