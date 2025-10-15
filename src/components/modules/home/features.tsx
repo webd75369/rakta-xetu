cff192e735ac37d22d3eab0da981eb9aacfd59f0
Nilav Talukdar <nilavtalukdar06@gmail.com>
2025-10-15 19:57:59 +0530

    added features section

--- src/components/modules/home/features.tsx
+++ src/components/modules/home/features.tsx
@@ -1,198 +1,32 @@
+import { Button } from "@/components/ui/button";
+import { features } from "@/lib/constants";
+import Link from "next/link";
+
 export function Features() {
   return (
     <div className="w-full my-6">
       <div className="grid sm:grid-cols-2 lg:grid-cols-4 items-center gap-2">
-        <a
-          className="group flex flex-col justify-center hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 rounded-xl p-4 border"
-          href="#"
-        >
-          <div className="flex justify-center items-center size-12 bg-blue-600 rounded-xl">
-            <svg
-              className="shrink-0 size-6 text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
+        {features.map((feature, index) => (
+          <Link
+            key={index}
+            className="group flex flex-col justify-center hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 rounded-lg p-4 border shadow-xs"
+            href={feature.path}
+          >
+            <Button
+              variant="secondary"
+              size="icon"
+              className="pointer-events-none"
             >
-              <rect width="10" height="14" x="3" y="8" rx="2" />
-              <path d="M5 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2h-2.4" />
-              <path d="M8 18h.01" />
-            </svg>
-          </div>
-          <div className="mt-5">
-            <h3 className="group-hover:text-gray-600 text-lg font-semibold text-gray-800">
-              Responsive
-            </h3>
-            <p className="mt-1 text-gray-600">
-              Responsive, and mobile-first project on the web
-            </p>
-            <span className="mt-2 inline-flex items-center gap-x-1.5 text-sm text-blue-600 decoration-2 group-hover:underline group-focus:underline font-medium">
-              Learn more
-              <svg
-                className="shrink-0 size-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="m9 18 6-6-6-6" />
-              </svg>
-            </span>
-          </div>
-        </a>
-        <a
-          className="group flex flex-col justify-center hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 rounded-xl p-4"
-          href="#"
-        >
-          <div className="flex justify-center items-center size-12 bg-blue-600 rounded-xl">
-            <svg
-              className="shrink-0 size-6 text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M20 7h-9" />
-              <path d="M14 17H5" />
-              <circle cx="17" cy="17" r="3" />
-              <circle cx="7" cy="7" r="3" />
-            </svg>
-          </div>
-          <div className="mt-5">
-            <h3 className="group-hover:text-gray-600 text-lg font-semibold text-gray-800">
-              Customizable
-            </h3>
-            <p className="mt-1 text-gray-600">
-              Components are easily customized and extendable
-            </p>
-            <span className="mt-2 inline-flex items-center gap-x-1.5 text-sm text-blue-600 decoration-2 group-hover:underline group-focus:underline font-medium">
-              Learn more
-              <svg
-                className="shrink-0 size-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="m9 18 6-6-6-6" />
-              </svg>
-            </span>
-          </div>
-        </a>
-        <a
-          className="group flex flex-col justify-center hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 rounded-xl p-4"
-          href="#"
-        >
-          <div className="flex justify-center items-center size-12 bg-blue-600 rounded-xl">
-            <svg
-              className="shrink-0 size-6 text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
-              <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
-            </svg>
-          </div>
-          <div className="mt-5">
-            <h3 className="group-hover:text-gray-600 text-lg font-semibold text-gray-800">
-              Documentation
-            </h3>
-            <p className="mt-1 text-gray-600">
-              Every component and plugin is well documented
-            </p>
-            <span className="mt-2 inline-flex items-center gap-x-1.5 text-sm text-blue-600 decoration-2 group-hover:underline group-focus:underline font-medium">
-              Learn more
-              <svg
-                className="shrink-0 size-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="m9 18 6-6-6-6" />
-              </svg>
-            </span>
-          </div>
-        </a>
-        <a
-          className="group flex flex-col justify-center hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 rounded-xl p-4"
-          href="#"
-        >
-          <div className="flex justify-center items-center size-12 bg-blue-600 rounded-xl">
-            <svg
-              className="shrink-0 size-6 text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M14 9a2 2 0 0 1-2 2H6l-4 4V4c0-1.1.9-2 2-2h8a2 2 0 0 1 2 2v5Z" />
-              <path d="M18 9h2a2 2 0 0 1 2 2v11l-4-4h-6a2 2 0 0 1-2-2v-1" />
-            </svg>
-          </div>
-          <div className="mt-5">
-            <h3 className="group-hover:text-gray-600 text-lg font-semibold text-gray-800">
-              24/7 Support
-            </h3>
-            <p className="mt-1 text-gray-600">
-              Contact us 24 hours a day, 7 days a week
-            </p>
-            <span className="mt-2 inline-flex items-center gap-x-1.5 text-sm text-blue-600 decoration-2 group-hover:underline group-focus:underline font-medium">
-              Learn more
-              <svg
-                className="shrink-0 size-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="m9 18 6-6-6-6" />
-              </svg>
-            </span>
-          </div>
-        </a>
+              <feature.icon />
+            </Button>
+            <div className="mt-5">
+              <h3 className="group-hover:text-gray-600 text-neutral-600">
+                {feature.label}
+              </h3>
+              <p className="mt-1 text-sm font-light text-neutral-500">{feature.description}</p>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
