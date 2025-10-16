663750c696129ff596454dda1f74c4b458201bcd
Nilav Talukdar <nilavtalukdar06@gmail.com>
2025-10-16 18:42:20 +0530

    added request form component

---
+++ src/app/(root)/(routes)/request-blood/new/page.tsx
@@ -0,0 +1,22 @@
+import { RequestForm } from "@/components/modules/request/request-form";
+import { Button } from "@/components/ui/button";
+import { HeartPulse } from "lucide-react";
+
+export default function NewRequest() {
+  return (
+    <div className="min-h-svh w-full flex justify-center items-center overflow-x-hidden p-4">
+      <div className="max-w-md w-full mx-auto">
+        <div className="w-full flex flex-col justify-center items-center gap-y-3">
+          <Button variant="secondary" className="pointer-events-none">
+            <HeartPulse />
+          </Button>
+          <h1 className="text-2xl text-neutral-600 font-light">RaktaXetu</h1>
+          <p className="text-neutral-400 font-light text-center">
+            Make a new blood request
+          </p>
+        </div>
+        <RequestForm />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/modules/request/request-form.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export function RequestForm() {
+  return (
+    <form className="my-6">
+      <p className="text-neutral-500 text-center font-light">Request Form</p>
+    </form>
+  );
+}
